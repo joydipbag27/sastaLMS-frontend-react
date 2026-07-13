4277e1c1da26ea1631ad06a388cc70bc65442cf9
joydipbag27 <joydipbag27@gmail.com>
2026-07-13 16:00:36 +0000

    chore: add shebang to deploy script and remove redundant delete flag from s3 sync command

--- deploy.sh
+++ deploy.sh
@@ -1,3 +1,5 @@
+#!/usr/bin/env bash
+
 set -e
 
 source .env.production
@@ -6,6 +8,6 @@ echo "Building app..."
 npm run build
 
 echo "Uploading to s3..."
-aws s3 sync dist/ s3://sastalms.sbs --delete --profile veolms-deployer --delete
+aws s3 sync dist/ s3://sastalms.sbs --delete --profile veolms-deployer
 
 echo "Deployment completed!!!"
